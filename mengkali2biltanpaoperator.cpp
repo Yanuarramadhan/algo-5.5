3d905d540d350ddf43ec57b211a964387e7d0947
Yanuar <45526227+Yanuarramadhan@users.noreply.github.com>
2018-12-13 16:48:10 +0700

    codingnya

---
+++ mengkali2biltanpaoperator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,y,z=0;
+    cout<<"bilangan pertmanya\n:"<<endl;
+    cin>>x;
+    cout<<"bilangan keduanya\n:"<<endl;
+    cin>>y;
+    for ( int i=1; i<= y;i++)
+    {
+        z+=y;
+
+    }cout<<"\n"<<x<<"di kalikan"<<y<<"adalah:"<<z<<endl;
+
+}
